cc0f9a919023b2c9e5933f7440c69618b6ac65a2
Jack Cullen <jpcullen20@gmail.com>
2020-03-31 09:16:54 -0600

    Add methods to read apartment information

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -3,4 +3,21 @@ class Apartment
   def initialize(information)
     @information = information
   end
+
+  def number
+    @information[:number]
+  end
+
+  def monthly_rent
+    @information[:monthly_rent]
+  end
+
+  def bathrooms
+    @information[:bathrooms]
+  end
+
+  def bedrooms
+    @information[:bedrooms]
+  end
+
 end
