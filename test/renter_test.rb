8596de9e43a3208229be5e4f9571f2e08c8daf79
Jack Cullen <jpcullen20@gmail.com>
2020-03-31 09:10:28 -0600

    Add attr_reader for renters name

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -11,4 +11,8 @@ class RenterTest < Minitest::Test
   def test_it_exists
     assert_instance_of Renter, @renter1
   end
+
+  def test_it_has_a_name
+    assert_equal "Jessie", @renter1.name
+  end
 end
