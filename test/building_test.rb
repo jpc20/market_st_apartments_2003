b33c8e256063780ed38315f986016f8dbf57eab4
Jack Cullen <jpcullen20@gmail.com>
2020-03-31 09:26:21 -0600

    Create and test add_unit method

--- test/building_test.rb
+++ test/building_test.rb
@@ -8,9 +8,22 @@ class BuildingTest < Minitest::Test
 
   def setup
     @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
   end
 
   def test_it_exists
     assert_instance_of Building, @building
+  end
+
+  def test_units_empty_by_default
+    assert_equal [], @building.units
+  end
+
+  def test_add_unit
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+
+    assert_equal [@unit1, @unit2], @building.units
   end
 end
